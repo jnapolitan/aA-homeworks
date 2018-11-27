bf932f72872cddfc74de601aaa44f78eab743c56
Julian Napolitan <julian.napolitan@gmail.com>
2018-11-27 13:11:02 -0800

    Finish AJAX request

--- W6D3/js/index.js
+++ W6D3/js/index.js
@@ -1,5 +1,11 @@
 console.log("Hello from the JavaScript console!");
 
-// Your AJAX request here
+$.ajax({
+    url: 'http://api.openweathermap.org/data/2.5/weather?q=new%20york,US&appid=bcb83c4b54aee8418983c2aff3073b3b',
+    type: 'GET',
+    success: function(response) {
+        console.log(response.weather[0]);
+    }
+});
 
-// Add another console log here, outside your AJAX request
+console.log("Hello again from the console!");
