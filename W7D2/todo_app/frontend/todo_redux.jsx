7bc6ff6a35352456ead353e5eeae455a16edb94a
Julian Napolitan <julian.napolitan@gmail.com>
2018-12-03 21:51:22 -0800

    Add logger middleware

---
+++ W7D2/todo_app/frontend/todo_redux.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+
+import Root from './components/root';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const preloadedState = localStorage.state ?
+    JSON.parse(localStorage.state) : {};
+  const store = configureStore(preloadedState);
+  store.dispatch = addLoggingToDispatch(store);
+  const root = document.getElementById('content');
+  ReactDOM.render(<Root store={store} />, root);
+});
+
+const addLoggingToDispatch = (store) => {
+  const storeDispatch = store.dispatch;
+  return (action) => {
+    console.log('Before action:', store.getState());
+    console.log('Action called:', action);
+    storeDispatch(action);
+    console.log('After action:', store.getState());
+  };
+};
